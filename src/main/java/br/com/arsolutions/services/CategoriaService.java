1e7dc54a65c316093cece7f9518760a89921ce89
André Garrido <andreluis226@hotmail.com>
2021-01-19 14:05:33 -0300

    Ajustes no EndPoint/categorias/{id}

--- src/main/java/br/com/arsolutions/services/CategoriaService.java
+++ src/main/java/br/com/arsolutions/services/CategoriaService.java
@@ -7,15 +7,27 @@ import org.springframework.stereotype.Service;
 
 import br.com.arsolutions.domain.Categoria;
 import br.com.arsolutions.repositories.CategoriaRepository;
+import br.com.arsolutions.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-
+	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+//	public Categoria find(Integer id) {
+//		Optional<Categoria> obj = repo.findById(id);
+//		if (obj == null) {
+//			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null);
+//
+//		}
+//		return obj;
+//	}
 
 }

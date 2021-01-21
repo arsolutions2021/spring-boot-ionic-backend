8376c5964a0ea11d369e6b3016ec8f160f03b057
André Garrido <andreluis226@hotmail.com>
2021-01-21 15:58:30 -0300

    Deletando uma categoria

--- src/main/java/br/com/arsolutions/services/CategoriaService.java
+++ src/main/java/br/com/arsolutions/services/CategoriaService.java
@@ -3,10 +3,12 @@ package br.com.arsolutions.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.arsolutions.domain.Categoria;
 import br.com.arsolutions.repositories.CategoriaRepository;
+import br.com.arsolutions.services.exceptions.DataIntegrityException;
 import br.com.arsolutions.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -24,6 +26,24 @@ public class CategoriaService {
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		
+			repo.deleteById(id);
+			
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+			
+		}
+		
 	}
 
 //	public Categoria find(Integer id) {

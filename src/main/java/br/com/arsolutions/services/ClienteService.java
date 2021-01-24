b117f69b933966bafb8d0733b0efbf60e7a3d3ea
André Garrido <andreluis226@hotmail.com>
2021-01-24 19:54:05 -0300

    Inserindo um cliente com Post

--- src/main/java/br/com/arsolutions/services/ClienteService.java
+++ src/main/java/br/com/arsolutions/services/ClienteService.java
@@ -1,12 +1,27 @@
 package br.com.arsolutions.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+
+import br.com.arsolutions.domain.Cidade;
 import br.com.arsolutions.domain.Cliente;
+import br.com.arsolutions.domain.Endereco;
+import br.com.arsolutions.domain.enums.TipoCliente;
+import br.com.arsolutions.dto.ClienteDTO;
+import br.com.arsolutions.dto.ClienteNewDTO;
+//import br.com.arsolutions.repositories.CidadeRepository;
 import br.com.arsolutions.repositories.ClienteRepository;
+import br.com.arsolutions.repositories.EnderecoRepository;
+import br.com.arsolutions.services.exceptions.DataIntegrityException;
 import br.com.arsolutions.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -14,20 +29,82 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
-	
+
+//	@Autowired
+//	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 
-//	public Cliente find(Integer id) {
-//		Optional<Cliente> obj = repo.findById(id);
-//		if (obj == null) {
-//			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName(), null);
-//
-//		}
-//		return obj;
-//	}
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+
+			repo.deleteById(id);
+
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque a entidades relacionadas");
+
+		}
+
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		return repo.findAll(pageRequest);
+
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 
 }

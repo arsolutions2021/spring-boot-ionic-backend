a17b61ba8b82a0f0448da53c6c138114fdd6cb80
André Garrido <andreluis226@hotmail.com>
2021-01-27 13:36:55 -0300

    produção

--- src/main/java/br/com/arsolutions/config/DevConfig.java
+++ src/main/java/br/com/arsolutions/config/DevConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.arsolutions.services.DBService;
 
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class DevConfig {
 
 	@Autowired

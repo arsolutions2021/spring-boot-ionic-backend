8633acd1def72567ffbe77965f669b87a4edfbd6
André Garrido <andreluis226@hotmail.com>
2021-01-27 15:55:43 -0300

    Implementando o toString de Pedido

--- src/main/java/br/com/arsolutions/domain/Pedido.java
+++ src/main/java/br/com/arsolutions/domain/Pedido.java
@@ -1,8 +1,11 @@
 package br.com.arsolutions.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -138,11 +141,13 @@ public class Pedido implements Serializable {
 
 	@Override
 	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		StringBuilder builder = new StringBuilder();
 		builder.append("Pedido número: ");
 		builder.append(getId());
 		builder.append(", Instante: ");
-		builder.append(getInstante());
+		builder.append(sdf.format(getInstante()));
 		builder.append(", Cliente: ");
 		builder.append(getCliente().getNome());
 		builder.append(", Situação do Pagamento: ");
@@ -152,7 +157,7 @@ public class Pedido implements Serializable {
 			builder.append(ip.toString());
 		}
 		builder.append("Valor total:");
-		builder.append(getValorTotal());
+		builder.append(nf.format(getValorTotal()));
 
 		return builder.toString();
 	}

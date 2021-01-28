6b2cb9b48ba2350dd6227a729402fae61279ebcf
André Garrido <andreluis226@hotmail.com>
2021-01-27 22:18:17 -0300

    Configuracao do Spring

--- src/main/java/br/com/arsolutions/services/MockEmailService.java
+++ src/main/java/br/com/arsolutions/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package br.com.arsolutions.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -12,6 +14,14 @@ public class MockEmailService extends AbstractEmailService {
 	public void sendEmail(SimpleMailMessage msg) {
 
 		LOG.info("Simulando Envio de E-mail.. ");
+		LOG.info(msg.toString());
+		LOG.info("E-mail Enviado..");
+		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando Envio de E-mail HMTL.. ");
 		LOG.info(msg.toString());
 		LOG.info("E-mail Enviado..");
 		
